0d18ecf4c7aaebcda3b41dfa031bf630849092b7
Oreoff <maximmelnik37@gmail.com>
2025-02-07 23:02:56 +0200

    Trying fix docker

--- src/GGStat-Web-Api/Program.cs
+++ src/GGStat-Web-Api/Program.cs
@@ -1,30 +1,60 @@
 using GGStat_Backend.data;
 using Microsoft.EntityFrameworkCore;
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<PlayersDBContext>(options =>
-	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddCors(options =>
+
+namespace GGStat_Backend
 {
-	options.AddPolicy("AllowLocalhost", policy =>
+	public class Program
 	{
-		policy.WithOrigins("http://localhost:3000") 
-			  .AllowAnyHeader() 
-			  .AllowAnyMethod(); 
-	});
-});
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+		public static void Main(string[] args)
+		{
+			// Створюємо об'єкт веб-застосунку
+			var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+			// Налаштування підключення до БД
+			builder.Services.AddDbContext<PlayersDBContext>(options =>
+				options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var app = builder.Build();
+			// Налаштування CORS для дозволу запитів з localhost:3000
+			builder.Services.AddCors(options =>
+			{
+				options.AddPolicy("AllowLocalhost", policy =>
+				{
+					policy.WithOrigins("http://localhost:3000")
+						  .AllowAnyHeader()
+						  .AllowAnyMethod();
+				});
+			});
 
-app.UseCors("AllowLocalhost");
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
+			// Налаштування Swagger
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			// Налаштування контролерів з Newtonsoft.Json
+			builder.Services.AddControllers().AddNewtonsoftJson();
+
+			var app = builder.Build();
+
+			// Міграції БД при старті застосунку
+			using (var scope = app.Services.CreateScope())
+			{
+				var dbContext = scope.ServiceProvider.GetRequiredService<PlayersDBContext>(); 
+			}
+
+			// Налаштування CORS
+			app.UseCors("AllowLocalhost");
+
+			// Налаштування Swagger в режимі розробки
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			// Налаштування маршрутизації
+			app.MapControllers();
+
+			// Запуск веб-застосунку
+			app.Run();
+		}
+	}
 }
-
-app.MapControllers();
-app.Run();
